3eb6627285056b64ac1e5191f591e24c03481703
Ichigo Kurosaki <42874053+JorgeFernandezJr@users.noreply.github.com>
2018-09-11 09:36:11 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,25 +5,3 @@ c = "COLEMAN"
 product = e ** pi 
 print (product)
 print(c,pi,e)
-
-
-
-# nums1.py
-
-x = 1000000000000000   # int
-y = 2.884684656851538  # float
-z = -5542+1j   # complex
-z2 = 6-22j #complex
-print ("integer, float, complex")
-print (x, y, z+z2)
-
-
-
-# var2.py jf
-a = 10 # integer (int) 
-b = -5 #integer (int)
-s2 = 1.141 # float
-s = "this is a string of characters" # string
-s3 = 'how about this' # string
-print (a,b,s2,s,s3)
-print ("the square root of 2 is " +str(s2))
