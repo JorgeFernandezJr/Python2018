08cbacaba44f74bb5b4ce16c3c9896f734487797
Ichigo Kurosaki <42874053+JorgeFernandezJr@users.noreply.github.com>
2018-09-11 09:22:48 -0700

    Add files via upload

---
+++ vars2.py
@@ -0,0 +1,8 @@
+# var2.py jf
+a = 10 # integer (int) 
+b = -5 #integer (int)
+s2 = 1.141 # float
+s = "this is a string of characters" # string
+s3 = 'how about this' # string
+print (a,b,s2,s,s3)
+print ("the square root of 2 is " +str(s2))
