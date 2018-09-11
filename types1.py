c4ff1bf2cd6b275dd93cef8c2994ebde80b151fc
Ichigo Kurosaki <42874053+JorgeFernandezJr@users.noreply.github.com>
2018-09-11 09:21:54 -0700

    Add files via upload

---
+++ types1.py
@@ -0,0 +1,12 @@
+# types1.py
+x = -1000
+y = 2.71828
+z = 3+1j
+s = "A"
+#print the variable values
+print (s,x,y,z)
+#print types
+print (type(s))
+print (type(x))
+print (type(y))
+print (type(z))
