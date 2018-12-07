94cd13430b041c071be488b7dfd48addff3862e5
Ichigo Kurosaki <42874053+JorgeFernandezJr@users.noreply.github.com>
2018-12-07 09:14:55 -0800

    Add files via upload

---
+++ python-checklist/ops1.py
@@ -0,0 +1,17 @@
+#ops.py jf
+"""
++	Addition	x + y	
+-	Subtraction	x - y	
+*	Multiplication	x * y	
+/	Division	x / y
+%	Modulus	x % y	
+**	Exponentiation	x ** y	 
+//	Floor division	x // y
+"""
+print ("33 + 77 =", 33 + 77)
+print ("33 - 77 =", 33 - 77)
+print ("33 * 77 =", 33 * 77)
+print ("33 / 77 =", 33 / 17)
+print ("33 % 77 =", 33 % 17)
+print ("33 ** 77 =", 33 ** 17)
+print ("33 // 77 =", 37 // 17)
